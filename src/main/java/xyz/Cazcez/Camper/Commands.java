cc7666c53d08797f9241de864a008440395f868e
mfnalex <1122571+mfnalex@users.noreply.github.com>
2020-07-13 02:12:45 +0200

    .

--- src/main/java/xyz/Cazcez/Camper/Commands.java
+++ src/main/java/xyz/Cazcez/Camper/Commands.java
@@ -3,13 +3,17 @@ package xyz.Cazcez.Camper;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
 import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.session.ClipboardHolder;
+import com.sk89q.worldedit.world.World;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -39,18 +43,18 @@ public class Commands implements CommandExecutor
 			try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
 				Clipboard clipboard = reader.read();
 
-				EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(p.getWorld(), -1);
+				try(EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(new BukkitWorld(p.getWorld()), -1)) {
 					// We have to get the world obect as WorldEdit world, not as Spigot world, but how?
 					Operation operation = new ClipboardHolder(clipboard)
 							.createPaste(editSession)
-							.to(BlockVector3.at(p.getLocation().getBlockX(),p.getLocation().getBlockY(),p.getLocation().getBlockZ()))
+							.to(BlockVector3.at(p.getLocation().getBlockX(), p.getLocation().getBlockY(), p.getLocation().getBlockZ()))
 							.ignoreAirBlocks(false)
 							.build();
 					Operations.complete(operation);
 
+				}
 
-
-		} catch (IOException ignored) {
+		} catch (IOException | WorldEditException ignored) {
 			sender.sendMessage("Error");
 			//e.printStackTrace();
 		}
