183ca7eb0e49687dd7cfd00e2a555b9671e53859
mfnalex <1122571+mfnalex@users.noreply.github.com>
2020-07-13 02:03:27 +0200

    .

--- src/main/java/xyz/Cazcez/Camper/Commands.java
+++ src/main/java/xyz/Cazcez/Camper/Commands.java
@@ -1,13 +1,20 @@
 package xyz.Cazcez.Camper;
 
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
+import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.session.ClipboardHolder;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,16 +32,30 @@ public class Commands implements CommandExecutor
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+		Player p = (Player) sender;
+
 		File file = new File(main.getDataFolder()+ File.separator+"test.schematic");
 		ClipboardFormat format = ClipboardFormats.findByFile(file);
-		try {
 			try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
 				Clipboard clipboard = reader.read();
-			}
+
+				EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(p.getWorld(), -1);
+					// We have to get the world obect as WorldEdit world, not as Spigot world, but how?
+					Operation operation = new ClipboardHolder(clipboard)
+							.createPaste(editSession)
+							.to(BlockVector3.at(p.getLocation().getBlockX(),p.getLocation().getBlockY(),p.getLocation().getBlockZ()))
+							.ignoreAirBlocks(false)
+							.build();
+					Operations.complete(operation);
+
+
+
 		} catch (IOException ignored) {
 			sender.sendMessage("Error");
 			//e.printStackTrace();
 		}
+
+
 
 		return true;
 	}
